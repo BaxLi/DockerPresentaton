4bb7d4b40d8260c25a5b18f4310ec7805d113075
BaxLi <lilian.baxan@gmail.com>
2023-11-06 11:30:36 +0200

    Step-1 need check

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def serve_image():
     return send_file('Demo.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
